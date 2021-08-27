09bb707ce765f6286c60337fb6e9be7cb9e8cab2
fmcch <fmhcry@gmail.com>
2021-08-27 15:26:45 +0200

    5. Website (list post)

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import Web3 from 'web3';
-import logo from '../logo.png';
+import Identicon from 'identicon.js';
 import './App.css';
+import SocialNetwork from '../abis/SocialNetwork.json';
 import Navbar from './Navbar';
 
 class App extends Component {
@@ -26,14 +27,36 @@ class App extends Component {
 
   async loadBlockchainData() {
     const web3 = window.web3;
+    
     const accounts = await web3.eth.getAccounts();
     this.setState({ account: accounts[0] });
+
+    const networkId = await web3.eth.net.getId();
+    const networkData = SocialNetwork.networks[networkId];
+    if(networkData) {
+      const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networkData.address);
+      this.setState({ socialNetwork });
+      const postCount = await socialNetwork.methods.postCount().call();
+      this.setState({ postCount });
+
+      for (var i = 1; i <= postCount; i++) {
+        const post = await socialNetwork.methods.posts(i).call();
+        this.setState({
+          posts: [...this.state.posts, post]
+        });
+      }
+    } else {
+      window.alert('SocialNetwork not deployed to detected network.');
+    }
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      account: ''
+      account: '',
+      socialNetwork: null,
+      postCount: 0,
+      posts: []
     }
   }
 
@@ -43,27 +66,37 @@ class App extends Component {
        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '500px' }}>
               <div className="content mr-auto ml-auto">
-                <a
-                  href="https://github.com/fmcch"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Bumstead’s dev - Work in progress</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://github.com/fmcch"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  My github
-                </a>
+                { this.state.posts.map((post, key) => {
+                  return(
+                  <div className="card mb-4" key={key}>
+                    <div className="card-header">
+                      <img className="mr-2"
+                        width='30'
+                        height='30'
+                        src={`data:image/png;base64,${new Identicon(post.author, 30).toString()}`}
+                      />
+                      <small className="text-muted">{post.author}</small>
+                    </div>
+                    <ul id="postList" className="list-group list-group-flush">
+                      <li className="list-group-item">
+                        <p>{post.content}</p>
+                      </li>
+                      <li key={key} className="list-group-item py-2">
+                        <small className="float-left mt-1 text-muted">
+                          TIPS: {window.web3.utils.fromWei(post.tipAmount.toString(), 'Ether')} ETH
+                        </small>
+                        <button className="btn btn-link btn-sm float-right pt-0">
+                          <span>
+                            TIP 0.1 ETH
+                          </span>
+                        </button>
+                      </li>
+                    </ul>
+                  </div>
+                  )
+                })}
               </div>
             </main>
           </div>
